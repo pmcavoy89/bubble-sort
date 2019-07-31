959118edaa361c9c67be36f5e429963881b2de14
Patrick <pjmwdb@mail.missouri.edu>
2019-07-30 20:41:43 -0400

    Move the variables to the bubble sort function instead of being in the global scope.

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,4 +1,6 @@
 function bubbleSort(setOfNumbers) {
+    let i, j, temp;
+
     for (i = 0; i < setOfNumbers.length - 1; i++) {
         for (j = 0; j < setOfNumbers.length - i - 1; j++) {
             if(setOfNumbers[j] > setOfNumbers[j+1]) {
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const bubbleSort = require('./bubble-sort');
 
 let setOfNumbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-let i, j, temp;
 
 console.log('The set of numbers = ', setOfNumbers);
 
